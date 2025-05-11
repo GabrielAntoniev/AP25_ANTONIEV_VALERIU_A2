4cf4305147734e45cf40d6d8ecb88c03e7ca16e8
gabi <gabrielantoniev@gmail.com>
2025-05-11 09:43:57 +0300

    Commit Message

--- lab5/src/main/java/org/example/Report.java
+++ lab5/src/main/java/org/example/Report.java
@@ -21,35 +21,29 @@ public class Report implements Command {
 
         String targetRepoName = args[1];
 
-        // Find repository
         Repository repository = repositoryList.stream()
                 .filter(r -> r.getName().equalsIgnoreCase(targetRepoName))
                 .findFirst()
                 .orElseThrow(() -> new RepositoryNotFoundException("Repository not found: " + targetRepoName));
 
         try {
-            // FreeMarker config
             Configuration cfg = new Configuration(Configuration.VERSION_2_3_31);
             cfg.setDirectoryForTemplateLoading(new File(System.getProperty("user.dir")+ File.separator + "src" +  File.separator + "main" + File.separator + "resources"));
             cfg.setDefaultEncoding("UTF-8");
 
             System.out.println(cfg.getTemplate("report-template.html"));
 
-            // Load template
             Template template = cfg.getTemplate("report-template.html");
 
-            // Data model
             Map<String, Object> data = new HashMap<>();
             data.put("repositoryName", repository.getName());
             data.put("images", repository.getContent()); // List<Image>
 
-            // Output file
             File outputFile = new File(System.getProperty("user.dir")+ File.separator + "src" +  File.separator + "main" + File.separator + "resources"+File.separator + "report.html");
             try (Writer writer = new FileWriter(outputFile)) {
                 template.process(data, writer);
             }
 
-            // Open in browser
             if (Desktop.isDesktopSupported()) {
                 Desktop.getDesktop().browse(outputFile.toURI());
             }
